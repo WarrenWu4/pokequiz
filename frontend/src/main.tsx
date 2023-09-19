4c81054a4a06c5398cb89841070ad64700b667ab
WarrenWu4 <warrenweiwu04@gmail.com>
2023-09-19 15:52:58 -0500

    added some more boilerplate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
 import './index.css'
+import Start from './pages/Start.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <Start/>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}  />
   </React.StrictMode>,
 )
+
+const url = import.meta.env.VITE_BACKEND_URL
+export default url
---
+++ frontend/src/pages/Start.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import url from "../main";
+
+const Start = () => {
+
+    const [data, setData] = useState<string>()
+
+    useEffect(() => {
+        let subscribed = true;
+
+        const getData = async() => {
+            if (subscribed) {
+                const response = await fetch(url+"/test");
+                const json = await response.json();
+                setData(json.data);
+            }
+        }
+        
+        getData();
+
+        return () => {
+            subscribed = false;
+        }
+    }, [])
+
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            {data}
+        </div>
+    )
+}
+
+export default Start
