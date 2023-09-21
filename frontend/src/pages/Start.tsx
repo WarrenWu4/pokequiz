b0389344263f771b7972b87b593461ad3bd308f9
WarrenWu4 <warrenweiwu04@gmail.com>
2023-09-21 15:19:13 -0500

    small update

--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -4,6 +4,7 @@ import url from "../main";
 const Start = () => {
 
     const [data, setData] = useState<string>()
+    const [pin, setPin] = useState<string>("0000");
 
     useEffect(() => {
         let subscribed = true;
@@ -24,9 +25,17 @@ const Start = () => {
         }
     }, [])
 
+    const handleForm = () => {
+        console.log("submitting form...")
+    }
+
     return (
-        <div className="w-screen h-screen flex items-center justify-center">
+        <div className="w-screen h-screen flex flex-col items-center justify-center p-4">
             {data}
+            <form onSubmit={handleForm} className="max-w-[320px] max-h-[400px] bg-gray-800 rounded-lg flex items-center justify-center flex-col p-4">
+                <label className="mb-2">Enter Game Pin:</label>
+                <input className="bg-transparent px-2 py-1 text-center" type="text" value={pin} onChange={(e) => setPin(e.target.value)}/>
+            </form>
         </div>
     )
 }
