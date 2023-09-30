62604a4257054afbb4e63d2487a63e9849d86d5e
WarrenWu4 <warrenweiwu04@gmail.com>
2023-09-30 17:25:54 -0500

    changed fetch route for about and start, added styling and basic structure for about, and added loading component

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react"
 import url from "../main";
+import Loading from "../components/Loading";
+
+interface Developer {
+    name: string;
+    year: string;
+    major: string;
+    about: string;
+}
 
 const About = () => {
 
     const [loaded, setLoaded] = useState<boolean>(false)
-    const [data, setData] = useState<string>("")
+    const [data, setData] = useState<Developer[]>([])
 
     useEffect(() => {
 
@@ -13,9 +21,9 @@ const About = () => {
         const getData = async() => {
             if (subscribed) {
                 console.log("fetching data in About.tsx ...")
-                const response = await fetch(url+"/warrenwu");
+                const response = await fetch(url+"/developers");
                 const json = await response.json();
-                setData(JSON.stringify(json))
+                setData(json)
                 setLoaded(true)
             }
         }
@@ -29,8 +37,23 @@ const About = () => {
     }, [])
 
     return (
-        <div className="text-white">
-            {loaded ? <div>{data}</div> : <div>Loading...</div>}
+        <div className="w-full h-full p-4 text-white">
+            {loaded ?
+                <div className="w-full h-full flex p-12 gap-x-12">
+                    {data.map((dev:Developer) => {
+                        return (
+                            <div key={dev.name}>
+                                <h1>{dev.name}</h1>
+                                <h2>{dev.year}</h2>
+                                <h2>{dev.major}</h2>
+                                <p>{dev.about}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                :
+                <Loading/>
+            }
         </div>
     )
 }
