f2d921e7981e0dc492d16367f616c47947fb9965
Sugam Mishra <sugammishra@Sugams-MacBook-Pro-2.local>
2023-10-19 17:14:15 -0500

    Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,9 +6,7 @@ const Navbar = () => {
 
     return (
         <div className="w-full flex justify-end items-center p-12 gap-x-4 [&>*]:cursor-pointer">
-            <div className="w-9 aspect-square"><MdBookmarks size={36}/></div>
-            <div className="w-9 aspect-square"><MdSettings size={36}/></div>
-            <div className="w-9 aspect-square bg-white rounded-md"></div>
+            <img src="navbar.png"/>
         </div>
     )
 }
