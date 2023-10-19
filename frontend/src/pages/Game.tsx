83385fc04e9181ac48a653e015567ddcb1a395c7
Ahmed Idrees <ahmedqidrees@client-10-229-166-204.tamulink.tamu.edu>
2023-10-19 17:34:43 -0500

    Finish starting page

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,5 +1,13 @@
+import Navbar from "../components/Navbar";
+
 const Game = () => {
-    return <h1>yoyoyoyoyoyoo</h1>;
+    return (
+        <div className="w-screen h-screen flex flex-col bg-[url('./stolen-image.png')] bg-center bg-no-repeat bg-cover">
+            <div className="w-full h-full flex flex-col backdrop-blur-sm">
+                <Navbar />
+            </div>
+        </div>
+    );
 };
 
 export default Game;
--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import url from "../main";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
 
 const Start = () => {
     const [data, setData] = useState<string>("");
@@ -39,31 +38,29 @@ const Start = () => {
     };
 
     return (
-        <div className="w-screen h-screen flex flex-col bg-[url('./stolen-image.png')] bg-center bg-no-repeat bg-cover">
-            <div className="w-full h-full flex flex-col backdrop-blur-sm">
+        <div className="w-screen h-screen flex flex-col bg-[url('bg-overlay.png')] bg-center bg-no-repeat bg-cover">
+            <div className="w-full h-full flex flex-col">
                 <Navbar />
-                <div className="absolute w-full h-full flex items-center justify-center flex-col p-4">
+                <div className="absolute w-full h-full flex items-center justify-center flex-col p-4 ">
                     <form
                         onSubmit={handleForm}
                         method="POST"
                         action={url + "/validate"}
-                        className="max-w-[320px] max-h-[400px] bg-white rounded-md flex items-center justify-center flex-col p-4 gap-y-4 shadow-lg border"
+                        className="bg-gradient-to-b from-[#5A6988] from-[53%] to-[#3A4466] to-[47%] rounded-sm flex items-center justify-center flex-col p-12 gap-y-4"
                     >
+                        <img
+                            src="/tree.png"
+                            className="w-[181.56px] h-[207.5px]"
+                        />
+                        <label className="text-center font-bold text-3xl">
+                            Enter Game Pin:
+                        </label>
                         <input
-                            placeholder="Game PIN"
-                            className="px-2 py-1 text-center bg-white rounded-md border placeholder-gray placeholder-opacity-50 select-none text-gray-700"
+                            className="px-5 py-1 bg-[#3A4466] text-center rounded-sm placeholder-gray placeholder-opacity-50 select-none text-gray-700 border-4"
                             type="text"
                             value={pin}
                             onChange={handleInput}
                         />
-                        <Link
-                            to={"/game/" + pin}
-                            className="w-full h-full bg-red-500 rounded-md border-gray flex justify-center items-center"
-                        >
-                            <button type="submit" className="px-2 py-1">
-                                Enter
-                            </button>
-                        </Link>
                     </form>
                 </div>
             </div>
