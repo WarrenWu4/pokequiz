d317873b3dbb7c5e935c214b7c1b9ca642934654
WarrenWu4 <warrenweiwu04@gmail.com>
2023-10-19 19:48:56 -0500

    progress for stuff

--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -32,50 +32,42 @@ const Start = () => {
     };
 
     const handleInput = (e: any) => {
-        if (e.target.value.length <= 4) {
+        if (e.target.value.length <= 12) {
             setPin(e.target.value);
         }
     };
 
     return (
-<<<<<<< HEAD
-        <div className="w-screen h-screen flex flex-col">
+    <div className="w-screen h-screen overflow-x-hidden bg-[url('bg-image.svg')] bg-center bg-no-repeat bg-cover">
+    
+        <div className="w-full h-full backdrop-blur-sm bg-black/80 flex flex-col items-center">
+
             <Navbar/>
-            <div className="w-full h-full flex items-center justify-center flex-col p-4">
-                
-                <form onSubmit={handleForm} method="POST" action={url+"/validate"} className="max-w-[320px] max-h-[400px] bg-gray-800 rounded-lg flex items-center justify-center flex-col p-4">
-                    <label className="mb-2">Enter Game Pin:</label>
-                    <input className="bg-transparent px-2 py-1 text-center" type="text" name="pin" value={pin} onChange={handleInput}/>
-                </form>
-=======
-        <div className="w-screen h-screen flex flex-col bg-[url('bg-overlay.png')] bg-center bg-no-repeat bg-cover">
-            <div className="w-full h-full flex flex-col">
-                <Navbar />
-                <div className="absolute w-full h-full flex items-center justify-center flex-col p-4 ">
-                    <form
-                        onSubmit={handleForm}
-                        method="POST"
-                        action={url + "/validate"}
-                        className="bg-gradient-to-b from-[#5A6988] from-[53%] to-[#3A4466] to-[47%] rounded-sm flex items-center justify-center flex-col p-12 gap-y-4"
-                    >
-                        <img
-                            src="/tree.png"
-                            className="w-[181.56px] h-[207.5px]"
-                        />
-                        <label className="text-center font-bold text-3xl">
-                            Enter Game Pin:
-                        </label>
-                        <input
-                            className="px-5 py-1 bg-[#3A4466] text-center rounded-sm placeholder-gray placeholder-opacity-50 select-none text-gray-700 border-4"
-                            type="text"
-                            value={pin}
-                            onChange={handleInput}
-                        />
-                    </form>
+
+            <div className="w-full h-full flex flex-col items-center justify-center">
+
+                <div className="w-full h-[400px] flex flex-col items-center justify-center px-4 relative">
+                    <div className="w-full h-[198px] bg-[#5A6988] rounded-t-md">
+                    </div>
+                    <div className="w-full h-[202px] bg-[#3A4466] top-[198px] rounded-b-md">
+                    </div>
+
+                    <div className="absolute top-[20px] flex flex-col items-center justify-center">
+                        <img src="/tree.png"/>
+                        <form onSubmit={handleForm} method="POST" action={url + "/validate"} className="flex flex-col gap-y-[10px] items-center mt-[12px]">
+                            <label className="font-bold text-[32px]">Enter Game Pin</label>
+                            <input type="text" className="outline-none px-2 w-[244px] h-[42px] bg-transparent border-4 border-[#FAF8ED] border-solid rounded-md font-semibold" value={pin} onChange={handleInput}/>
+                        </form>
+                    </div>
                 </div>
->>>>>>> 83385fc04e9181ac48a653e015567ddcb1a395c7
+
             </div>
+
+
+
         </div>
+
+    </div>
     );
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,6 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
-            backgroundImage: {
-                "about-image":
-                    "url('https://www.desktopbackground.org/download/1920x1080/2011/12/04/307118_all-pokemon-wallpapers-hd_3888x2459_h.jpg')",
-            },
         },
     },
     plugins: [],
