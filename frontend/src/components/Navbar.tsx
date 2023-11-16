6d8103610cd7ef05d0b2e65380aa0bc57b5e8ef0
WarrenWu4 <warrenweiwu04@gmail.com>
2023-11-16 16:57:58 -0600

    reorganized assets, finished navbar & edited start form sizing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,11 +4,17 @@ const Navbar = () => {
     return (
         <div className="w-full mt-[10px] min-h-[56px] px-4 flex justify-end items-center gap-x-3 [&>*]:cursor-pointer bg-transparent">
             <div className="w-10 aspect-square rounded-full bg-[#F9B572] border-2 border-solid border-white flex justify-center items-center">
-                <img src="/bookmark.png"/>
+                <img src="/icons/shopping-cart.svg"/>
+            </div>
+            <div className="w-10 aspect-square rounded-full bg-[#F9B572] border-2 border-solid border-white flex justify-center items-center">
+                <img src="/icons/bookmark.svg"/>
             </div>
             <div className="w-10 aspect-square rounded-full bg-[#F9B572] border-2 border-solid border-white">
+                <img src="/icons/gear.svg"/>
             </div>
-            <div className="w-10 aspect-square rounded-full bg-[#F9B572] border-2 border-solid border-white"></div>
+            <div className="w-10 aspect-square rounded-full bg-[#F9B572] border-2 border-solid border-white">
+                <img src="/icons/slime.svg"/>
+            </div>
         </div>
     );
 };
--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -58,7 +58,7 @@ const Start = () => {
 
             <div className="w-full h-full flex flex-col items-center justify-center">
 
-                <div className="w-full h-[400px] flex flex-col items-center justify-center px-4 relative">
+                <div className="max-w-[475px] w-full h-[400px] flex flex-col items-center justify-center px-4 relative">
                     <div className="w-full h-[198px] bg-[#5A6988] rounded-t-md">
                     </div>
                     <div className="w-full h-[202px] bg-[#3A4466] top-[198px] rounded-b-md">
