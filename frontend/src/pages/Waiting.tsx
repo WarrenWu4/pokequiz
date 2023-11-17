2edd3708af6164075e9671ff47ad6cf9e0d02fdc
WarrenWu4 <warrenweiwu04@gmail.com>
2023-11-16 18:34:42 -0600

    made waiting responsive

--- frontend/src/pages/Waiting.tsx
+++ frontend/src/pages/Waiting.tsx
@@ -25,32 +25,27 @@ const Waiting = () => {
         }
     }, [])
 
-    const handleForm = () => {
-        console.log("submitting form...")
-    }
-
 
     return (
-        <div className="w-screen h-screen flex flex-col bg-[url('/bg-image.png')] bg-cover bg-no-repeat">
-            <Navbar/>
-            <div className="w-full h-full flex items-center justify-center flex-col p-4 backdrop-blur-md">
-                
-                <form onSubmit={handleForm} method="POST" action={url+"/validate"} className="bg-[url('/woodenSign.png')] h-[280px] w-[275px] rounded-lg absolute top-0 flex items-center justify-center flex-col p-4 md:blur-none">
-                    <label className="mb-2">Game Pin:</label>
-                    <label className="mb-2">Username</label>
-                </form>
-                <div className=" bg-[url('/depth(-1).png')] bg-contain bg-no-repeat w-full h-[50px] absolute bottom-0">
-                </div>
-                <div className=" bg-[url('/ground.png')] bg-contain bg-no-repeat w-full h-[80px] absolute bottom-0">
-                <div>
-                    <a href="https://pokemondb.net/pokedex/charizard"><img src="https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" alt="Charizard" className="absolute bottom-[75px] left-[50px]"/></a>
-                    <a href="https://pokemondb.net/pokedex/venusaur"><img src="https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur-f.gif" alt="Venusaur" className="absolute bottom-[75px] left-[150px]"/></a>
-                    <a href="https://pokemondb.net/pokedex/blastoise"><img src="https://img.pokemondb.net/sprites/black-white/anim/normal/blastoise.gif" alt="Blastoise" className="absolute bottom-[75px] left-[250px]"/></a>                 
-                </div>
-                
-                </div>
+        <div className="w-screen h-screen overflow-x-hidden bg-[url('bg-image.svg')] bg-center bg-no-repeat bg-cover">
+    
+            <div className="w-full h-full backdrop-blur-sm bg-black/80 flex flex-col items-center">
+            
+                <Navbar/>
+                    
+                    <div className="bg-[url('/wooden-sign.svg')] h-[50%] w-[50%] bg-contain bg-center px-4 bg-no-repeat mt-24 flex justify-center items-center flex-wrap p-8">
+                        askldjfalsdjfaksldfjasf
+                    </div>
+                    <div className=" bg-[url('/tile006.png')] bg-contain w-full h-[40px] absolute bottom-0">
+                    </div>
+                    <div className="bg-[url('/tile002.png')] bg-contain w-full h-[40px] absolute bottom-[40px]">
+                    <div className="absolute w-full flex justify-center bottom-[35px]">
+                        <img src="https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" alt="Charizard"/>
+                        <img src="https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur-f.gif" alt="Venusaur"/>
+                        <img src="https://img.pokemondb.net/sprites/black-white/anim/normal/blastoise.gif" alt="Blastoise"/>         
+                    </div>
+                    </div>
             </div>
-
         </div>
     )
 }
